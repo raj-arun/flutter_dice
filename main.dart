1130ceb57cd01c148257048f5a8709ced4752ff6
ARUN RAJ <s.arun.raj@gmail.com>
2020-04-26 19:18:33 -0400

    Fixed the yellow stripe bar in portrait mode
    Fixed the yellow stripe bar in portrait mode

---
+++ main.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftValue = 1;
+  int rightValue = 2;
+
+  void changeDice() {
+    setState(() {
+      leftValue = Random().nextInt(6) + 1;
+      rightValue = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: null,
+                    child: Image.asset('images/dice$leftValue.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: null,
+                    child: Image.asset('images/dice$rightValue.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.play_circle_outline),
+                tooltip: 'Click to Roll the Dice',
+                iconSize: 48.0,
+                color: Colors.white,
+                onPressed: () {
+                  changeDice();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
